5cee771f22e4610c848412128dcdac30adcffa31
My Name <my-name@chromium.org>
2021-07-16 03:22:38 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,41 +6,39 @@ import Homepage from './Homepage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
- 
-  const[title,setTitle] = useState('');
-    const[description,setDescription] = useState('');
-    const[duedate,setDuedate] = useState('');
-    const[status,setStatus] = useState('');
 
-    const help=(a,b,c,d)=>{
-      setTitle(a);
-      setDescription(b);
-      setDuedate(c);
-      setStatus(d);
-      console.log(a,b,c,d);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [duedate, setDuedate] = useState('');
+  const [status, setStatus] = useState('');
 
-    }
+  const help = (a, b, c, d) => {
+    setTitle(a);
+    setDescription(b);
+    setDuedate(c);
+    setStatus(d);
+    console.log(a, b, c, d);
+
+  }
 
   return (
     <div className="App">
-     
-     
-    <BrowserRouter>
-   <Switch>
-     <Route exact path= "/" >
-     <Homepage help={help}/>
-     </Route>
-     <Route exact path= "/confirm">
-       <Confirmpage title={title} des={description} due={duedate} status={status}/>
 
-      
-     </Route>
-   </Switch>
 
-    </BrowserRouter>
-     
-       
-        
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" >
+            <Homepage preity={help} />
+          </Route>
+          <Route exact path="/preity">
+            <Confirmpage title={title} des={description} due={duedate} status={status} />
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
